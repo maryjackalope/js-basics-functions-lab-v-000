b918aa2d3d6c6026f54b983691b8c586d4f3531a
Mary Jack <maryjackinthebox@gmail.com>
2018-11-04 20:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location){
+  //returns a distance in blocks 
+  //calculates distances below 42nd street
+  let distance;
+  if (location >= 42){
+    distance = location - 42;
+  }
+  else {
+    distance = 42 - location;
+  }
+   return distance;
+}
+
+
+/*function distanceFromHqInFeet(location) {
+  //returns a distance in feet 
+  //calculates distances below 42nd street
+  distanceFromHqInBlocks(location) *264; 
+}
+*/
+
+function distanceTravelledInFeet(begin, end) {
+  
+  
+}
+
+function calculatesFarePrice(start, destination) {
+ 
+ let distance = destination - start;
+  
+  //gives customers a free sample (first 400ft free)
+  //if destination - start < 4000 return "It's free"
+  //charges 2 cents per foot when total feet travelled is //between 400 and 2000 (remember the first 400 feet are free!)
+  //if destination - start
+  //charges 25 dollars for a distance over 2000 feet
+  //no rides over 2500
+  if
+}
+
